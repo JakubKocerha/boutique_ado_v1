66a6a35884dd12747af5958dae91ac7a73893607
JakubKocerha <jakubbubble@gmail.com>
2022-02-20 20:01:29 +0000

    complete webhook handler

--- bag/views.py
+++ bag/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect, reverse, HttpResponse, get_object
 from django.contrib import messages
 
 from products.models import Product
+
 # Create your views here.
 
 def view_bag(request):
-    """ A view that renders the bag contents page  """
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
@@ -41,7 +42,7 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
+    
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
@@ -75,7 +76,7 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """  Remove the item from the shopping bag """
+    """Remove the item from the shopping bag"""
 
     try:
         product = get_object_or_404(Product, pk=item_id)
@@ -88,7 +89,7 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-                messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
